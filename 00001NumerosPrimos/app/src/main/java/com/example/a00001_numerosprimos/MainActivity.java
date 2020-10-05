2c02b510295a9b94fd73da4caf528c34050d94f3
Dyango <dsanchezr@fpcoruna.afundacion.org>
2020-10-05 11:40:38 +0200

    Clase:05/10/2020

---
+++ 00001NumerosPrimos/app/src/main/java/com/example/a00001_numerosprimos/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.a00001_numerosprimos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
