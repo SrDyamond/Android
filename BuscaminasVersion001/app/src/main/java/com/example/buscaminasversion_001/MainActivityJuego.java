3effce5face9a68d4882b86e08fe70240a8748f2
SrDyamond <dyangosanchezrodriguez@gmail.com>
2020-11-06 21:18:25 +0100

    "Creacion tablero,aun sin funcionar"

--- BuscaminasVersion001/app/src/main/java/com/example/buscaminasversion_001/MainActivityJuego.java
+++ BuscaminasVersion001/app/src/main/java/com/example/buscaminasversion_001/MainActivityJuego.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivityJuego extends AppCompatActivity {
     private TextView niv;
-    private int filas =0, columnas=0,minas=0,totalceldas=0,casilla=0;
+    private int filas =0, columnas=0,minas=0,totalceldas=0;
+    private LinearLayout contenedor;
 
 
     @Override
@@ -24,6 +27,7 @@ public class MainActivityJuego extends AppCompatActivity {
 
         niv=(TextView)findViewById(R.id.nivel);
         niv.setText(fraseimportada.toString());
+        contenedor=(LinearLayout)findViewById(R.id.container);
         this.crearTablero(fraseimportada);
     }
     public void atras(View v) {
@@ -53,6 +57,22 @@ public class MainActivityJuego extends AppCompatActivity {
             totalceldas = 480;
         }
         Toast.makeText(this, "Creando tablero", Toast.LENGTH_SHORT).show();
+
+        for (int i = 0; i < totalceldas; i++) {
+            Button bn = new Button(getApplicationContext());
+            bn.setId(1 + 2000); //Define ids
+            bn.setMinHeight(0);
+            bn.setMinimumHeight(0);
+            bn.setPadding(0,0,0,0); //Elimina padding
+            bn.setTextSize(14);
+            //Crea contenedor con peso de 1 para cada elemento.
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f);
+              bn.setLayoutParams(params);
+            //Agrega vista a contenedor padre.
+            contenedor.addView(bn);
+        }
+
+
     }
 
 }
